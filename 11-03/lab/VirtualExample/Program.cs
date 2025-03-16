35b53e2fa4ab8b9946617e5883b7e091115735c0
Yasin Maden <maden.yasin@hotmail.com>
2025-03-16 22:17:34 +0300

    11-03

---
+++ 11-03/lab/VirtualExample/Program.cs
@@ -0,0 +1,32 @@
+﻿Obje obje = new Obje();
+Kalem kalem = new Kalem();
+Terlik terlik = new Terlik();
+
+
+Console.WriteLine(obje.Bilgi());
+Console.WriteLine(kalem.Bilgi());
+Console.WriteLine(terlik.Bilgi());
+
+public class Obje
+{
+    public virtual string Bilgi()
+    {
+        return "Ben bir objeyim.";
+    }
+}
+
+public class Terlik : Obje
+{
+    override public string Bilgi()
+    {
+        return "Ben bir terliğim.";
+    }
+}
+
+public class Kalem : Obje
+{
+    public override string Bilgi()
+    {
+        return "Ben bir kalemim";
+    }
+}
---
+++ 11-03/lab/VirtualExample02/Program.cs
@@ -0,0 +1,30 @@
+﻿Ucgen ucgen = new Ucgen() { Boy = 3, En = 4};
+Dİkdortgen dİkdortgen = new Dİkdortgen() { Boy = 3, En = 4};
+Console.WriteLine(ucgen.AlanHesapla());
+Console.WriteLine(dİkdortgen.AlanHesapla());
+
+public class Sekil
+{
+    public int Boy { get; set; }
+    public int En { get; set; }
+
+    public virtual int AlanHesapla()
+    {
+        return 0;
+    }
+}
+
+public class Ucgen : Sekil
+{
+    override public int AlanHesapla()
+    {
+        return En * Boy / 2;
+    }
+}
+public class Dİkdortgen : Sekil
+{
+    override public int AlanHesapla()
+    {
+        return En * Boy;
+    }
+}
